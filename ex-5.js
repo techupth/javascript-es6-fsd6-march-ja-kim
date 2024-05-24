8bf05a62bf3ef61472de6000e15d69cf5b197d46
PNKim <paljapon.nait@gmail.com>
2024-05-24 12:31:22 +0700

    feat: resolve ex-5

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,10 @@
-function sum() {
+function sum(...input) {
   // Start coding here !
+  let total = 0;
+  for (let i of input) {
+    total += i;
+  }
+  return total;
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
